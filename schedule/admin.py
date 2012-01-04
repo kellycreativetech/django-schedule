a9b4387a98bbe4e97cce652aa20f3e35f7bde5dc
Josh Boles <github@joshboles.com>
2012-01-04 13:34:56 -0500

    Overriding the date time field.

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from schedule.forms import RuleForm
+from django.db import models
 
-from schedule.models import Calendar, Event, CalendarRelation, Rule
+from schedule.models import Calendar, Event, Rule
+from utils.widgets import JqSplitDateTimeWidget
+from utils.fields import JqSplitDateTimeField
 
 class CalendarAdminOptions(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
@@ -10,6 +13,11 @@ class CalendarAdminOptions(admin.ModelAdmin):
 class RuleAdmin(admin.ModelAdmin):
     form = RuleForm
 
+class EventAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.DateTimeField: {'widget': JqSplitDateTimeField(widget=JqSplitDateTimeWidget(attrs={'date_class':'datepicker','time_class':'timepicker'}))},
+    }
+
 admin.site.register(Calendar, CalendarAdminOptions)
 admin.site.register(Rule, RuleAdmin)
-admin.site.register([Event, CalendarRelation])
+admin.site.register(Event, EventAdmin)
