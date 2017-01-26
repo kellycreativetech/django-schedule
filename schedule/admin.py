426414ce786b3d73b1003811845a7eb4f5fa48d0
Kasey Kelly <kasey@servee.com>
2017-01-26 12:43:55 -0500

    cleaning up the admin form

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from schedule.forms import RuleForm, EventAdminForm
 from django.db import models
+from django.conf import settings
 
-from schedule.models import Calendar, Event, CalendarRelation, Rule
+from schedule.models import Calendar, Event, CalendarRelation, Rule, Occurrence
+
+class OccurrenceAdmin(admin.ModelAdmin):
+
+    list_display = [
+        'title', 'start', 'end'
+    ]
 
 
 class CalendarAdminOptions(admin.ModelAdmin):
@@ -16,8 +23,13 @@ class RuleAdmin(admin.ModelAdmin):
 
 class EventAdmin(admin.ModelAdmin):
     form = EventAdminForm
+    list_display = [
+        'title',
+        'start',
+    ]
 
 admin.site.register(Calendar, CalendarAdminOptions)
 admin.site.register(Rule, RuleAdmin)
 admin.site.register(CalendarRelation)
 admin.site.register(Event, EventAdmin)
+admin.site.register(Occurrence, OccurrenceAdmin)
